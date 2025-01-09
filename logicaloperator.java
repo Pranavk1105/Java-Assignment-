efe342653a7a3408c432fbe03eee1c400c905b2f
Pranavk1105 <pranavkale2806@gmail.com>
2025-01-09 23:58:28 +0530

    Add files via upload

---
+++ logicaloperator.java
@@ -0,0 +1,16 @@
+//WAP to demonstrate logical operators in java.
+
+class logicaloperator{
+    public static void main (String [] args){
+        boolean a = true ;
+        boolean b = false;
+
+        System.out.println("a && b: " +(a && b));
+        System.out.println( " a || b: "+ (a || b));
+        System.out.println( " !a : " + (!a));
+        
+        
+
+
+    }
+}
