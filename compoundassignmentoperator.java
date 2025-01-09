0e2a4be8799b14e11d2ecec27fe2e514d7fc45cb
Pranavk1105 <pranavkale2806@gmail.com>
2025-01-09 23:58:06 +0530

    Add files via upload

---
+++ compoundassignmentoperator.java
@@ -0,0 +1,13 @@
+//WAP to demonstrate compound assignment operator.
+
+class compoundassignmentoperator{
+    public static void main(String []args){
+        int b = 11;
+        b += 6;
+        System.out.println(" b after += " + b);
+        b -= 5;
+        System.out.println("b after -= " + b );
+
+    
+    }
+}
