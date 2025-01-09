78edf9413a5bee72d32c19e97b9df50bc5b4344b
Pranavk1105 <pranavkale2806@gmail.com>
2025-01-09 23:58:14 +0530

    Add files via upload

---
+++ relationaloperators.java
@@ -0,0 +1,14 @@
+//WAP to demonstrate relational operators in java.
+
+class relationaloperators {
+    public static void main(String [] args){
+        int a = 34;
+        int b = 32;
+        System.out.println(" a > b :" + (a>b) );
+        System.out.println("a < b :" + (a<b));
+        System.out.println( "a == b : " +(a==b));
+        System.out.println( " a!=b : " +(a!=b ));
+    }
+
+}
+
