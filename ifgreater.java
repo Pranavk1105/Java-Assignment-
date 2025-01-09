0d541c64a62366f3a697a6836c1318d9625ef318
Pranavk1105 <pranavkale2806@gmail.com>
2025-01-09 23:59:01 +0530

    Add files via upload

---
+++ ifgreater.java
@@ -0,0 +1,15 @@
+//WAP to find greater of two numbers using if else statement.
+
+class ifgreater{
+    public static void main(String []args){
+        int a = 56;
+        int b = 67;
+
+     if (a>b){
+     System.out.println("a is greater than b");
+     }
+     else{
+     System.out.println("b is greater than a");
+     }
+    }
+}
