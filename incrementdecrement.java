92671801374711d0fe0af4d89de32ae5615199bb
Pranavk1105 <pranavkale2806@gmail.com>
2025-01-09 23:57:54 +0530

    Add files via upload

---
+++ incrementdecrement.java
@@ -0,0 +1,9 @@
+class incrementdecrement {
+    public static void main (String [] args) {
+        int a = 45;
+        System.out.println(" pre-increment operator is " + (++a));
+        System.out.println(" post-increament operator is " +(a++));
+        System.out.println(" pre-decreament operator is " + (--a));
+        System.out.println(" post-decreament operator is " + (a--));
+    }
+}
