f371715da7ed8eb49b54c7367644678277f2bdfa
Pranavk1105 <pranavkale2806@gmail.com>
2025-01-09 23:57:42 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,16 @@
+ 
+
+
+ class arithmatic {
+    public static void main(String []args){
+       int a = 5;
+       int b = 2;
+       System.out.println("Addition of two number of is " +(a+b));
+       System.out.println("Subtraction of two number of is " +(a-b));
+       System.out.println("Multiplication of two number is " + (a*b));
+       System.out.println("Division of two number is " + (a/b));
+
+
+
+    }
+ }
