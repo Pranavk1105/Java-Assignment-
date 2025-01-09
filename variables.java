486d425983f727d8fa3d3c0e8befaeab90e9542a
Pranavk1105 <pranavkale2806@gmail.com>
2025-01-09 23:57:16 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,11 @@
+//Q7 WAP to demonstrate how to create variables of different types.
+
+class variables {
+    public static void main(String []args) {
+    int age = 18;
+    float height = 170.1f;
+    boolean statement = true;
+
+    System.out.println("Age is " + age + " \nheight " + height + " \nstatement " + statement);
+    }
+}
