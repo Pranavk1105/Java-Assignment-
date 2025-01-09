f50932e7c2b7d45a9283c11c4a9bdafef33214c1
Pranavk1105 <pranavkale2806@gmail.com>
2025-01-09 23:58:50 +0530

    Add files via upload

---
+++ conditionaloperator.java
@@ -0,0 +1,11 @@
+//WAP to find greater of two numbers using conditional operator.
+
+class conditionaloperator {
+    public static void main(String []args){
+        double a = 30.1;
+       double b = 56.2;
+       double greater = (a>b)? a:b;
+       System.out.println("the greater number is " + greater );
+
+    }
+}
