97283d3c78f8ae7921df0ebef61cebe27435c318
Pranavk1105 <pranavkale2806@gmail.com>
2025-01-09 23:57:28 +0530

    Add files via upload

---
+++ explicit.java
@@ -0,0 +1,10 @@
+//WAP to demonstrate implicit type conversion.
+
+class explicit{
+    public static void main (String [] args){
+        double a =6.77;
+        int b = (int) a;
+        System.out.println("implicit type conversion " + b);
+    }
+
+}
